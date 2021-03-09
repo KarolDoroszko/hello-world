62df011bdc3b722fb9818f10bf09c3e60543ddfb
KarolDoroszko <doroszko.karol@gmail.com>
2021-03-09 20:46:55 +0100

    Added Inter class/module And interfaces. As I wanted to learn about strategy (inclusion)

--- try1.py
+++ try1.py
@@ -1,2 +1,30 @@
-print('Hello world!')
-print('Hello, I have just extended thr script')
+import Inter
+class Duck:
+    def __init__(self, Dtype, Dname,Dflying):
+        self.Dtype = Dtype
+        self.Dname = Dname
+        self.Dflying = Dflying
+    def Dflying_meth(self):
+        if self.Dtype == 'NoFlying':
+            self.Dflying.INoflying()
+        if self.Dtype == 'NormalFlying':
+            self.Dflying.IFlying()
+        if self.Dtype == 'ExtraFlying':
+            self.Dflying.IExtraFlying(100)
+
+if __name__ == '__main__':
+    print('Hello world!')
+    print('Hello, I have just extended thr script')
+    InterFLY = Inter.IFlying(50,50,50)
+    InterNOF = Inter.IFlying()
+    InterEXF = Inter.IFlying(100,100,100)
+
+    DuckNormal = Duck('NormalFlying','Flying Duck',InterFLY)
+    DuckExtra = Duck('ExtraFlying','Extra Flying Duck',InterEXF)
+    DuckNoFly = Duck('NoFlying','No Flying Duck',InterNOF)
+
+    DuckNoFly.Dflying_meth()
+    DuckExtra.Dflying_meth()
+    DuckNormal.Dflying_meth()
+
+
